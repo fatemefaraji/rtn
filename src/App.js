d29bc3a179c3962e430057537c330cd1e4125353
fatemefaraji <f.faraji54@umail.umz.ac.ir>
2025-06-14 21:52:48 +0330

    setting up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className=" font-sans">
-      <Navbar />
+     
+      <Home/>
 
     </div>
   );
