dc18eda81b38175bfc2af9133da12795d9df901e
fatemefaraji <f.faraji54@umail.umz.ac.ir>
2025-06-15 11:28:16 +0330

    setting up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from "react";
-import Home from "./pages/home/Home";
+import React from 'react';
+import Home from './pages/home/Home';
 
 function App() {
   return (
-    <div className=" font-sans">
-     
-      <Home/>
-
+    <div className="font-sans">
+      <Home />
     </div>
   );
 }
