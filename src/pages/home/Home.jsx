a08b0356a0bdbb35460e6c538ba8d75196e0bfb8
fatemefaraji <f.faraji54@umail.umz.ac.ir>
2025-06-16 22:13:11 +0330

    setting up

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <main className="p-8 text-center text-gray-700">
         <h1 className="text-4xl font-bold mb-4">Welcome to Our Blog!</h1>
         <p className="text-lg">
-          This is the main content area, positioned directly below the navbar.
+          
         </p>
       </main>
     </div>
