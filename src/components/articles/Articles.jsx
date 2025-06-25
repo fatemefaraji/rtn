048c1acaf38fa525be9163f6ab8c8880f34c650b
fatemefaraji <f.faraji54@umail.umz.ac.ir>
2025-06-25 10:31:45 +0330

    building artivle section

--- src/components/articles/Articles.jsx
+++ src/components/articles/Articles.jsx
@@ -1,16 +1,13 @@
-import art from "../../../src/assets/images/1715604853550.png"
+import art from "../../../src/assets/images/1715604853550.png";
+
 function Articles() {
-    return (
-<div className="bg-slate-200 w-52 h-64 py-4 px-4 border-sky-300 font-semibold my-10 mx-5 hover:cursor-pointer hover:bg-slate-300 text-center transition border-shadow">
-    <img src={art} className="w-full h-5/6 hover:border-fuchsia-500"></img>
-    <h3 className="text-left p-8 font-bold  ">Varaebels</h3>
-    <p className="text-left">
-        this is dummy text
-    </p>
-    
-
-
-</div>
-    )
+  return (
+    <div className="bg-slate-200 w-52 h-64 py-4 px-4 border-sky-300 font-semibold my-10 mx-5 hover:cursor-pointer hover:bg-slate-300 text-center transition shadow">
+      <img src={art} alt="Article Illustration" className="w-full h-5/6 hover:border-fuchsia-500" />
+      <h3 className="text-left p-2 font-bold">Variables</h3>
+      <p className="text-left">This is dummy text</p>
+    </div>
+  );
 }
+
 export default Articles;
