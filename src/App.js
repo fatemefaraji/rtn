9b2e146dd00c62eeffd61faa5122fcdfdf327bb9
fatemefaraji <f.faraji54@umail.umz.ac.ir>
2025-07-19 10:57:05 +0330

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/navbar/Navbar.jsx';
 import Home from './pages/home/Home.jsx';
 import AboutUS from './pages/aboutUs/AboutUS.jsx';
-import Navbar from '../src/components/navbar/Navbar.jsx';
 import ArticlePage from './pages/articlePage/ArticlePage.jsx';
 import Categories from './pages/categories/Categories.jsx';
 import Contact from './pages/contact/Contact.jsx';
@@ -10,17 +11,19 @@ import CreateContent from './pages/createContent/CreateContent.jsx';
 
 function App() {
   return (
-    <div className="font-sans">
+    <div className="font-sans bg-gray-50 min-h-screen">
       <Navbar />
 
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<AboutUS />} />
-        <Route path='/articles/:id' element={<ArticlePage />} /> {/* تغییر این خط */}
-        <Route path='/categories' element={<Categories />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/create-content' element={<CreateContent/>}/>
-      </Routes>
+      <main className="pt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUS />} />
+          <Route path="/articles/:id" element={<ArticlePage />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/create-content" element={<CreateContent />} />
+        </Routes>
+      </main>
     </div>
   );
 }
