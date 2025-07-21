e8f2fd19ff6a804ddd7808a9ed53b846277cbbed
fatemefaraji <f.faraji54@umail.umz.ac.ir>
2025-07-22 00:03:44 +0330

    Update routing and theme context usage in App.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import Home from './pages/home/Home.jsx';
 import AboutUS from './pages/aboutUs/AboutUS.jsx';
 import ArticlePage from './pages/articlePage/ArticlePage.jsx';
 import Categories from './pages/categories/Categories.jsx';
+import CategoryPage from './pages/categories/CategoryPage.jsx';
 import Contact from './pages/contact/Contact.jsx';
 import CreateContent from './pages/createContent/CreateContent.jsx';
 import Login from './pages/login/Login.jsx';
 import ProtectedRoute from './components/protectedRoute/ProtectedRoute.jsx';
 
 import { ThemeContext } from './context/ThemeContext';
-import CategoryPage from './pages/categories/CategoryPage.jsx';
 
 function App() {
   const { isDark } = useContext(ThemeContext);
@@ -27,9 +27,17 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutUS />} />
           <Route path="/articles/:id" element={<ArticlePage />} />
-          <Route path="/categories" element={<Categories />} />
+          <Route path="/categories" element={<Categories />} /> {/* صفحه لیست دسته‌ها */}
+          <Route path="/category/:category" element={<CategoryPage />} /> {/* صفحه مقالات دسته */}
           <Route path="/contact" element={<Contact />} />
-          <Route path="/create-content" element={<ProtectedRoute><CreateContent /></ProtectedRoute>} />
+          <Route
+            path="/create-content"
+            element={
+              <ProtectedRoute>
+                <CreateContent />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/login" element={<Login />} />
         </Routes>
       </main>
