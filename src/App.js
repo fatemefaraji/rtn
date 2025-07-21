3a3845363b1fcb3d55c9238316f48212f2135717
fatemefaraji <f.faraji54@umail.umz.ac.ir>
2025-07-21 17:15:55 +0330

    Refactor App.js to improve routing and layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/navbar/Navbar.jsx';
@@ -9,10 +9,17 @@ import Categories from './pages/categories/Categories.jsx';
 import Contact from './pages/contact/Contact.jsx';
 import CreateContent from './pages/createContent/CreateContent.jsx';
 import Login from './pages/login/Login.jsx';
+import ProtectedRoute from './components/protectedRoute/ProtectedRoute.jsx';
+
+import { ThemeContext } from './context/ThemeContext';
+import CategoryPage from './pages/categories/CategoryPage.jsx';
 
 function App() {
+  const { isDark } = useContext(ThemeContext);
+
   return (
-    <div className="font-sans bg-gray-50 min-h-screen">
+    <div className={`font-sans min-h-screen
+      ${isDark ? 'bg-gray-900 text-gray-200' : 'bg-gray-50 text-gray-900'}`}>
       <Navbar />
 
       <main className="pt-4">
@@ -22,8 +29,8 @@ function App() {
           <Route path="/articles/:id" element={<ArticlePage />} />
           <Route path="/categories" element={<Categories />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/create-content" element={<CreateContent />} />
-          <Route path='/login' element={<Login/>}/>
+          <Route path="/create-content" element={<ProtectedRoute><CreateContent /></ProtectedRoute>} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </main>
     </div>
